91170a2e4569d2526a5dacc069bd596d2b20bf33
Sebastian Waisbrot <sebastian@brooklynpacket.com>
2010-10-15 22:37:08 -0700

    Added a bunch of tests

--- server/spec/models/user_spec.rb
+++ server/spec/models/user_spec.rb
@@ -38,4 +38,53 @@ describe "User Model" do
     @user.save
     @user.should_not be_valid
   end
+
+  it "should not allow usernames with '@'" do
+    @user.attributes = valid_user_attributes.except(:new_password_confirmation)
+    @user.username = @user.email
+    @user.should_not be_valid
+  end
+
+  it "should allow valid user to sign up" do
+    @user.attributes = valid_user_attributes
+    @user.should be_valid
+  end
+
+  it "should not allow duplicated emails" do
+    @user.attributes = valid_user_attributes
+    @user.save
+    user2 = User.new
+    user2.attributes = valid_user_attributes.except(:email)
+    user2.email = 'example2@example.com'
+    user2.should_not be_valid
+    @user.destroy
+  end
+
+  it "should not allow duplicated usernames" do
+    @user.attributes = valid_user_attributes
+    @user.save
+    user2 = User.new
+    user2.attributes = valid_user_attributes.except(:username)
+    user2.username = 'test2'
+    user2.should_not be_valid
+    @user.destroy
+  end
+
+  it "should allowed registered user to signin" do
+    @user.attributes = valid_user_attributes
+    @user.save
+    user = User.authenticate(valid_user_attributes[:username], valid_user_attributes[:new_password])
+    user.should be_valid
+    @user.destroy
+  end
+
+  it "should not allowed registered user to signin with wrong password" do
+    @user.attributes = valid_user_attributes
+    @user.save
+    user = User.authenticate(valid_user_attributes[:username], '_fake_password_')
+    user.should be nil
+    @user.destroy
+  end
+
+  it "should add pending invites on signup"
 end
