ca6df8144e47c48463ac6a1f7fd9096f57c7f448
Sebastian Waisbrot <sebastian@brooklynpacket.com>
2010-10-16 11:21:58 -0700

    Test for campaign requiring name

--- server/spec/models/campaign_spec.rb
+++ server/spec/models/campaign_spec.rb
@@ -1,8 +1,27 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper.rb')
 
 describe "Campaign Model" do
-  let(:campaign) { Campaign.new }
+  include UserSpecHelper
+
+  before(:all) do
+    @user = User.new
+    @user.attributes = valid_user_attributes
+    @user.save
+  end
+
+  after(:all) do
+    @user.destroy
+  end
+
+  before(:each) do
+    @campaign = Campaign.new
+  end
+
   it 'can be created' do
-    campaign.should_not be_nil
+    @campaign.should_not be_nil
+  end
+
+  it 'should have a name' do
+    @campaign.should_not be_valid
   end
 end
